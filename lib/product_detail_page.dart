0cf81036766c135c2f49704c4ec270b083b3459a
fang69x <109065342+fang69x@users.noreply.github.com>
2025-02-09 22:43:35 +0530

    product detail page

--- lib/product_detail_page.dart
+++ lib/product_detail_page.dart
@@ -6,6 +6,10 @@ class ProductDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail page"),
+      ),
+    );
   }
 }
